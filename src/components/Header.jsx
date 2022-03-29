3acaf2c8c96907c82c2ec0ef4430650a4f611932
Imeh Usoro <imeusoro@rocketmail.com>
2022-03-29 18:15:45 +0100

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,71 @@
 import React, { Component } from "react";
-
+import { HeaderStyle } from "../styles/HeaderStyles";
+import { RiArrowDropDownLine, RiArrowDropUpLine } from "react-icons/ri";
 export class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currency: "$", showCurrency: false, showCart: false };
+  }
   render() {
-    return <div>Header</div>;
+    return (
+      <HeaderStyle>
+        {/* first */}
+        <ul>
+          <li className="active">Women</li>
+          <li>Men</li>
+          <li>Kids</li>
+        </ul>
+        {/* second */}
+        <div>
+          <img src="cart-logo.png" alt="" />
+        </div>
+        {/* third */}
+        <div className="cart">
+          <button
+            onClick={() => {
+              this.setState({ showCurrency: !this.state.showCurrency });
+            }}
+          >
+            <span>{this.state.currency}</span>
+            {this.state.showCurrency ? (
+              <RiArrowDropUpLine />
+            ) : (
+              <RiArrowDropDownLine />
+            )}
+            {this.state.showCurrency ? (
+              <div className="currency">
+                <button
+                  onClick={() => {
+                    this.setState({ currency: "$" });
+                  }}
+                >
+                  $ USD
+                </button>
+                <button
+                  onClick={() => {
+                    this.setState({ currency: "€" });
+                  }}
+                >
+                  € EUR
+                </button>
+                <button
+                  onClick={() => {
+                    this.setState({ currency: "¥" });
+                  }}
+                >
+                  ¥ JPY
+                </button>
+              </div>
+            ) : null}
+          </button>
+
+          <div>
+            <img src="shopping-cart.png" alt="cart" />
+            <span className="item-number">{2}</span>
+          </div>
+        </div>
+      </HeaderStyle>
+    );
   }
 }
 
--- src/styles/HeaderStyles.js
+++ src/styles/HeaderStyles.js
@@ -1,1 +1,85 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+export const HeaderStyle = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 1.5rem;
+  padding: 0rem 6.25rem;
+
+  ul {
+    list-style-type: none;
+    display: flex;
+    margin: 0;
+    padding: 0;
+    text-transform: uppercase;
+  }
+  li {
+    padding: 0.25rem 1rem 2rem 1rem;
+  }
+  & li.active {
+    color: #5ece7b;
+    border-bottom: 2px solid;
+    font-weight: 600;
+  }
+  div span {
+    font-weight: 500;
+    font-size: large;
+  }
+  & div.cart {
+    display: flex;
+    align-items: flex-start;
+    gap: 1.375rem;
+  }
+  & div.cart button {
+    padding: 0;
+    border: none;
+    background: none;
+    position: relative;
+    cursor: pointer;
+    display: flex;
+    align-items: flex-end;
+    /* border: 1px solid red; */
+  }
+  & div.cart div.currency {
+    position: absolute;
+    top: 1.4rem;
+    left: -1.25rem;
+    flex-direction: column;
+    width: 7.125rem;
+    filter: drop-shadow(0 10px 8px rgb(0 0 0 / 0.04))
+      drop-shadow(0 4px 3px rgb(0 0 0 / 0.1));
+  }
+  & div.cart div.currency button {
+    padding-top: 1.25rem;
+    padding-right: 2.5rem;
+    padding-left: 1.25rem;
+  }
+  & div.cart div {
+    display: flex;
+    position: relative;
+  }
+  & div.cart div span {
+    position: absolute;
+    top: -0.7rem;
+    right: -0.7rem;
+    background: black;
+    color: white;
+    width: 1.25rem;
+    height: 1.252rem;
+    border-radius: 100%;
+    font-family: "Roboto", sans-serif;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+/* div > a {
+  display: flex;
+  align-items: center;
+  color: red;
+} */
+
+/* border: 1px solid red; */
